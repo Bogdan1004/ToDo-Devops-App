8d9702afa67a689b7f55c7d4ae37df0bc0ed7f28
root <root@lenovo.localdomain>
2026-03-04 14:08:48 +0200

    Again

--- app/main.py
+++ app/main.py
@@ -45,7 +45,7 @@ def root():
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Todo App - DevOps Level Up</title>
+  <title>Todo App - DevOps Level Up Endava</title>
   <style>
     * { margin: 0; padding: 0; box-sizing: border-box; }
     body {
